6674b9eff3281f4a8c72fa717c46f446037e99d4
mGrachii <mmartinez09cost58@gmail.com>
2018-04-28 16:31:02 -0600

    otro text
    test

--- src/grachi/Grachi.java
+++ src/grachi/Grachi.java
@@ -19,6 +19,8 @@ public class Grachi {
         System.out.print("Hello");
         System.out.print("Hello");
         System.out.print(":'v");
+        System.out.print("poor i grach :'v");
+        
 
     }
     
